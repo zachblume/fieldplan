5feb668f5c7f4b49927bd0f599433968e4561a1b
Zach Blume <zachblume@gmail.com>
2022-10-03 16:48:50 +0000

    Database connection is working!

--- functions/index.js
+++ functions/index.js
@@ -1,17 +1,13 @@
-// The Cloud Functions for Firebase SDK to
-// create Cloud Functions and set up triggers.
+// The Cloud Functions for Firebase SDK.
 const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
     functions.logger.info("Hello logs!");
-    response.send("Hello from Firebase!");
+    response.send("Hello world");
 });
 
 // exports.scheduledFunction =
@@ -20,24 +16,27 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 //  return null;
 // });
 
-// SQLtoRealTimeChartTables
-// Will run every ___ (15?) minutes
-// Runs a series of SQL queries [for each account]
-// const Knex = require('knex');
-const createUnixSocketPool = require('./connect-unix.js');
-// const createUnixSocketPool = require('./pool.js');
-
 exports.SQLtoRealTimeChartTables =
     functions.https.onRequest((request, response) => {
 
-        // const pg = require('knex')({ client: 'pg' });
-        // response.send(knex.select("*").from("ContactHistory"));
-        knex.raw("SELECT 1").then(() => {
+        const Knex = require("knex");
+        const pool = Knex({
+            client: "pg",
+            connection: {
+                user: "postgres",
+                password: "easypassword",
+                database: "development",
+                host: "/cloudsql/campaign-data-project:us-east1:fieldplan",
+            }
+        });
+
+        pool.raw("SELECT * from ContactHistory LIMIT 100").then((result) => {
             console.log("PostgreSQL connected");
-            response.send("posgre conncted");
+            response.send(result);
         }).catch((e) => {
             console.log("PostgreSQL not connected");
             console.error(e);
             response.send(e);
         });
+
     });
