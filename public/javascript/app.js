dd9b063d6614cdd02be1b3ed521f48a4f235bdcb
Zach Blume <zachblume@gmail.com>
2022-10-03 20:08:43 +0000

    Fixing some JSON issues and getting frontend ready to display graph with charjs

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -241,10 +241,35 @@ async function getCustomClaimRole() {
 
 function LoggedInHomePageDisplay() {
     document.querySelector('#LoggedInUser').style.display = 'block';
-    var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
-    starCountRef.on('value', (snapshot) => {
-        const data = snapshot.val();
-        updateStarCount(postElement, data);
-    });    
-    
+    db.collection("data").doc("weeklycontacthistory")
+        .onSnapshot((doc) => {
+            var parseddata = JSON.parse(doc.data().resultstring);
+            console.log("Current data: ", parseddata);
+            updateGraph(parseddata);
+        });
+
+}
+
+function updateGraph(data) {
+    // Bar chart
+    new Chart(document.getElementById("bar-chart"), {
+        type: 'bar',
+        data: {
+            labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+            datasets: [
+                {
+                    label: "Population (millions)",
+                    backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+                    data: [2478, 5267, 734, 784, 433]
+                }
+            ]
+        },
+        options: {
+            legend: { display: false },
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050'
+            }
+        }
+    });
 }
